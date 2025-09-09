fbf56e74cb1b8dcb7d230acb7ed011f9321bad4e
Dakaro Mueller <LukarioMC@gmail.com>
2025-09-09 10:40:07 -0700

    Implemented map from tutorial

---
+++ src/Map.cpp
@@ -0,0 +1,83 @@
+#include "Map.h"
+#include "TextureManager.h"
+
+int mapData[10][15] = {
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+};
+
+Map::Map() : water(TextureManager::load("../asset/water.png")),
+             dirt(TextureManager::load("../asset/dirt.png")),
+             grass(TextureManager::load("../asset/grass.png"))
+{
+    load(mapData);
+
+    // What part of the images do we want?
+    src.x = src.y = 0;
+    src.w = src.h = 32;
+
+    // Where should they be displayed?
+    dest.x = dest.y = 0;
+    dest.w = dest.h = 64;
+}
+
+Map::~Map()
+{
+    if (water)
+        SDL_DestroyTexture(water);
+    if (dirt)
+        SDL_DestroyTexture(dirt);
+    if (grass)
+        SDL_DestroyTexture(grass);
+}
+
+void Map::load(int data[10][15])
+{
+    for (int row = 0; row < 10; row++)
+    {
+        for (int col = 0; col < 15; col++)
+        {
+            map[row][col] = data[row][col];
+        }
+    }
+}
+
+void Map::draw()
+{
+    int type = 0;
+
+    for (int row = 0; row < 10; row++)
+    {
+        for (int col = 0; col < 15; col++)
+        {
+            type = map[row][col];
+
+            dest.y = static_cast<float>(row) * 64;
+            dest.x = static_cast<float>(col) * 64;
+
+            switch (type)
+            {
+            case 0:
+                TextureManager::draw(water, src, dest);
+                break;
+            case 1:
+                TextureManager::draw(dirt, src, dest);
+                break;
+            case 2:
+                TextureManager::draw(grass, src, dest);
+                break;
+
+            default:
+                break;
+            }
+        }
+    }
+}
---
+++ src/Map.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "Game.h"
+
+class Map
+{
+public:
+    Map();
+    ~Map();
+
+    void load(int data[10][15]);
+    void draw();
+
+private:
+    // Brace initialization to make the struct values within all zereo'd
+    SDL_FRect src{}, dest{};
+    SDL_Texture *water = nullptr;
+    SDL_Texture *dirt = nullptr;
+    SDL_Texture *grass = nullptr;
+    SDL_Texture *ball = nullptr;
+
+    int map[10][15]{};
+};
